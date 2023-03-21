128e46ac40adcdc9422f4e89e73bf9a8eb18c534
Juhniel <juhn.sweden@gmail.com>
2023-03-21 12:25:37 +0100

    userprofile stuff

---
+++ target/classes/static/js/addToFavourites.js
@@ -0,0 +1,39 @@
+
+function addToFavourites(event) {
+    event.preventDefault(); // prevent the default form submission
+    // const heartIcon = document.querySelector(".addToFavorites i");
+    // const heartIcon = event.target.querySelector("i"); // get the heart icon inside the button
+
+    let heartIcon;
+    if (event.target.classList.contains("addToFavorites")) {
+        heartIcon = event.target.querySelector("i");
+    } else {
+        heartIcon = event.target;
+    }
+
+
+    const recipeId = event.target.closest(".addToFavorites").getAttribute('data-recipe-id'); // get the recipe ID from the data-attribute
+    fetch(`/favourites/add?recipeId=${recipeId}`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ recipeId: recipeId })
+    })
+        .then(response => response.json())
+        .then(data => {
+            if (data.success) {
+                // update the heart icon based on the response
+                if (heartIcon.classList.contains("fa-heart-o")) {
+                    heartIcon.classList.remove("fa-heart-o");
+                    heartIcon.classList.add("fa-heart");
+                } else {
+                    heartIcon.classList.remove("fa-heart");
+                    heartIcon.classList.add("fa-heart-o");
+                }
+            } else {
+                alert("Failed to add recipe to favorites.");
+            }
+        })
+        .catch(error => console.error(error));
+}
